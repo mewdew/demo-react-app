9398480e9e81339e63bff01a646c3d73f0902d8d
Shivangi Sinha <shivangi.sinha@maersk.com>
2024-04-04 19:08:54 +0530

    fix: search, add status in dummy data

--- src/App.js
+++ src/App.js
@@ -7,40 +7,44 @@ import CreateModal from './components/CreateModal';
 
 function App() {
   const [data, setData] = useState(initialData);
+  const [viewData, setViewData] = useState(initialData);
 
   const addItem = (item) => {
     setData([...data, item]);
+    setViewData([...viewData, item]);
   };
 
   const removeItem = (id) => {
     setData(data.filter((item) => item.id !== id));
+    setViewData(viewData.filter((item) => item.id !== id));
   };
 
   const searchItem = (query) => {
     if (!query) {
-      setData(data);
+      setViewData(data);
       return;
     }
-    setData(data.filter((item) => item.name.includes(query)));
+    const currentViewData = viewData.filter((item) => (item.name.toLowerCase()).includes(query.toLowerCase()));
+    setViewData(currentViewData);
   };
 
   return (
     <div className="App">
       <ActionBar />
       <SearchBar onSearchItem={searchItem} />
-      <List data={data} onRemoveItem={(id) => removeItem(id)}/>
+      <List data={viewData} onRemoveItem={(id) => removeItem(id)}/>
       <CreateModal onAddItem={addItem} />
     </div>
   );
 }
 
 export const initialData = [
-  {id: 1, name: 'Task 1'},
-  {id: 2, name: 'Task 2'},
-  {id: 3, name: 'Task 3'},
-  {id: 4, name: 'Task 4'},
-  {id: 5, name: 'Task 5'},
-  {id: 6, name: 'Task 6'},
+  {id: 1, name: 'Task 1', status: 'NEW'},
+  {id: 2, name: 'Task 2', status: 'NEW'},
+  {id: 3, name: 'Task 3', status: 'NEW'},
+  {id: 4, name: 'Task 4', status: 'NEW'},
+  {id: 5, name: 'Task 5', status: 'NEW'},
+  {id: 6, name: 'Task 6', status: 'NEW'},
 ];
 
 
